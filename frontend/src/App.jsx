9af6c9e90ac15d0d06a6f1f8987f1cbc6497df5a
LHY927 <liuhaiyue0927@gmail.com>
2024-04-18 13:05:28 +0200

    add TopicNavigationBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 
 import PhotoList from './components/PhotoList';
 import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 import './App.scss';
 
 const App = () => (
   < div className="App" >
-    < TopicList />
+    < TopNavigation />
     < PhotoList />
   </div >
 )
