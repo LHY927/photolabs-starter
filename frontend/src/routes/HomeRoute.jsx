600a95dd8ce93bb696c4ae5a8c6928084ad73751
LHY927 <liuhaiyue0927@gmail.com>
2024-04-18 13:49:21 +0200

    use mock data

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -4,12 +4,14 @@ import '../styles/HomeRoute.scss';
 
 import PhotoList from 'components/PhotoList';
 import TopNavigation from 'components/TopNavigationBar';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      < TopNavigation />
-      < PhotoList />
+      < TopNavigation topics={topics} />
+      < PhotoList photos={photos} />
     </div>
   );
 };
