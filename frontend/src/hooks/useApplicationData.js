57aee5f10f110197b5284012c0873d75cdaaf297
LHY927 <liuhaiyue0927@gmail.com>
2024-04-19 16:39:25 +0200

    initialize reducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,12 +1,4 @@
 const useApplicationData = () => {
-    const {
-      state,
-      onPhotoSelect,
-      updateToFavPhotoIds,
-      onLoadTopic,
-      onClosePhotoDetailsModal,
-    } = useApplicationData();
-
     const [favourites, setFavourite] = useState([]);
     const [selectedPhoto, setSelectedPhoto] = useState({
         "id": "-1",
@@ -25,4 +17,31 @@ const useApplicationData = () => {
         "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
         }
     });
+
+    const {
+        state,
+        onPhotoSelect,
+        updateToFavPhotoIds,
+        onLoadTopic,
+        onClosePhotoDetailsModal,
+      } = useApplicationData();
 }
+
+
+/* insert app levels actions below */
+export const ACTIONS = {
+    FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+    FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+    SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+    SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+    SELECT_PHOTO: 'SELECT_PHOTO',
+    DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
+  }
+  
+  function reducer(state, action) {
+    switch (action.type) {
+      case FAV_PHOTO_ADDED:
+        return { /* insert logic */ }
+      { /* insert all relevant actions as case statements*/ }  
+      }
+    }
