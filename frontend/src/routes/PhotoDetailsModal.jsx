9dd95487dd802aa7f68b0cde89f0f0bec2609118
LHY927 <liuhaiyue0927@gmail.com>
2024-04-19 14:53:26 +0200

    done similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useMemo } from 'react';
 
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
@@ -7,29 +7,24 @@ import PhotoFavButton from 'components/PhotoFavButton';
 import FavBadge from 'components/FavBadge';
 import PhotoList from 'components/PhotoList';
 
-let similarPhotos = {
-  photo1: {
+//Initialize value to avoid render fail in the similar photos
+let similarPhotosList = [{
+  "id": "1",
+  "location": {
+    "city": "Montreal",
+    "country": "Canada"
+  },
+  "urls": {
+    "full": `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
+    "regular": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`
+  },
+  "user": {
     "id": "1",
-    "location": {
-      "city": "Montreal",
-      "country": "Canada"
-    },
-    "urls": {
-      "full": `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
-      "regular": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`
-    },
-    "user": {
-      "id": "1",
-      "username": "exampleuser",
-      "name": "Joe Example",
-      "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-    }
+    "username": "exampleuser",
+    "name": "Joe Example",
+    "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
   }
-}
-
-let similarPhotosList = [];
-
-let similarPhotosDiv = ""
+}];
 
 const PhotoDetailsModal = (props) => {
   const handleClickClose = () => {
@@ -53,37 +48,18 @@ const PhotoDetailsModal = (props) => {
     console.log(props.selectedPhoto.similar_photos)
   }
 
-  useEffect(() => {
+  useMemo(() => {
     if (props.selectedPhoto.id > 0) {
-      similarPhotos = props.selectedPhoto.similar_photos
-
       similarPhotosList = [];
       for (const index in props.selectedPhoto.similar_photos) {
         similarPhotosList.push(props.selectedPhoto.similar_photos[index]);
       }
 
-      similarPhotosDiv = similarPhotosList.map((photo, index) =>
-        <img key={photo.id} src={photo.urls.regular} className='photo-details-modal__images' />
-      );
-      console.log("similarPhotos")
-      console.log(similarPhotosDiv)
+      console.log(photos)
+      console.log(similarPhotosList)
     }
 
   }, [props.selectedPhoto]);
-
-
-  // const similarPhotosList = [];
-  // for (const index in similarPhotos) {
-  //   similarPhotosList.push(similarPhotos[index]);
-  // }
-
-  // const similarPhotosDiv = similarPhotosList.map((photo, index) =>
-  //   <image key={photo.id} src={photo.urls.regular} className='similarPhotosDiv' />
-  // );
-
-  // const similarPhotos = props.selectedPhoto.similar_photos.map((photo, index) =>
-  //   <image key={photo.id} src={photo.urls.regular} />
-  // );
 
   return (
     <div className="photo-details-modal" hidden={props.selectedPhoto.id < 0}>
@@ -103,7 +79,7 @@ const PhotoDetailsModal = (props) => {
         </div>
         <div>Similar Photos</div>
         <div className='photo-details-modal__top-bar'>
-          <PhotoList photos={photos} />
+          <PhotoList photos={similarPhotosList} />
         </div>
       </div>
     </div>
